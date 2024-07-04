858a21abe3917bbbc60ffb1b333a124cbe221eb4
Ricardo Calvo <rcalvo70931@ufide.ac.cr>
2024-07-03 22:05:03 -0600

    Interfaz Gestion de Partidos

--- 2024_2C_SC-304_-GRUPO5/src/pkg2024_2c_sc/pkg304_/grupo5/Partido.java
+++ 2024_2C_SC-304_-GRUPO5/src/pkg2024_2c_sc/pkg304_/grupo5/Partido.java
@@ -9,5 +9,42 @@ package pkg2024_2c_sc.pkg304_.grupo5;
  * @author rcalv
  */
 public class Partido {
+
+    private String fecha;
+    private String equipoLocal;
+    private String equipoVisitante;
+
+    public Partido(String fecha, String equipoLocal, String equipoVisitante) {
+        this.fecha = fecha;
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEquipoLocal() {
+        return equipoLocal;
+    }
+
+    public void setEquipoLocal(String equipoLocal) {
+        this.equipoLocal = equipoLocal;
+    }
+
+    public String getEquipoVisitante() {
+        return equipoVisitante;
+    }
+
+    public void setEquipoVisitante(String equipoVisitante) {
+        this.equipoVisitante = equipoVisitante;
+    }
     
+    
+    
+
 }
--- 2024_2C_SC-304_-GRUPO5/src/pkg2024_2c_sc/pkg304_/grupo5/Resultado.java
+++ 2024_2C_SC-304_-GRUPO5/src/pkg2024_2c_sc/pkg304_/grupo5/Resultado.java
@@ -13,5 +13,37 @@ public class Resultado {
     private Partido partido;
     private int golesLocal;
     private int golesVisitante;
+
+    public Resultado(Partido partido, int golesLocal, int golesVisitante) {
+        this.partido = partido;
+        this.golesLocal = golesLocal;
+        this.golesVisitante = golesVisitante;
+    }
+
+    public Partido getPartido() {
+        return partido;
+    }
+
+    public void setPartido(Partido partido) {
+        this.partido = partido;
+    }
+
+    public int getGolesLocal() {
+        return golesLocal;
+    }
+
+    public void setGolesLocal(int golesLocal) {
+        this.golesLocal = golesLocal;
+    }
+
+    public int getGolesVisitante() {
+        return golesVisitante;
+    }
+
+    public void setGolesVisitante(int golesVisitante) {
+        this.golesVisitante = golesVisitante;
+    }
+    
+    
     
 }
